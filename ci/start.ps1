dbaca458f56bd90a7589609382a11e246e67e156
Adam Gensler <adam.gensler@calculi.com>
2019-05-14 11:48:01 -0700

    GX-3501 log processes on failure

--- ci/start.ps1
+++ ci/start.ps1
@@ -1,16 +1,14 @@
-#7z.exe e .\netdemo-server.zip
 Add-Type -assembly "system.io.compression.filesystem"
 $archiveName = "$env:PKG_BASE_DIR/$env:APPLICATION_NAME/netdemo-server.zip"
 Write-Output "Extracting Archive: $archiveName"
 [io.compression.zipfile]::ExtractToDirectory($archiveName, "$env:PKG_BASE_DIR/$env:APPLICATION_NAME")
 Write-Output "Done Extracting"
 
-#Start-Process .\netdemo-Server.exe
-
 $Command = "$env:PKG_BASE_DIR/$env:APPLICATION_NAME/netdemo-server.exe"
 Invoke-Expression $Command
 
 $healthprocess = Get-Process netdemo-Server -ErrorAction SilentlyContinue
 if ($null -eq $healthprocess) {
-	throw [System.Exception] "application not started"
+    Get-Process
+    throw [System.Exception] "application not started"
 }
