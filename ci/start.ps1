5f3fa9f5ff2e3a03782919f545835842ad47ecbf
Adam Gensler <adam.gensler@calculi.com>
2019-05-17 17:33:22 -0700

    GX-3501 install 7-Zip

--- ci/start.ps1
+++ ci/start.ps1
@@ -1,4 +1,9 @@
 Add-Type -assembly "system.io.compression.filesystem"
+
+# installing 7-Zip
+Invoke-WebRequest -OutFile 7z1900-x64.msi https://www.7-zip.org/a/7z1900-x64.msi
+Start-Process ".\7z1900-x64.msi" -ArgumentList "/quiet" -Wait
+
 $archiveName = "$env:PKG_BASE_DIR/$env:APPLICATION_NAME/netdemo-server.zip"
 Write-Output "Extracting Archive: $archiveName"
 [io.compression.zipfile]::ExtractToDirectory($archiveName, "$env:PKG_BASE_DIR/$env:APPLICATION_NAME")
@@ -8,7 +13,6 @@ $port = "8081"
 if ($null -ne $env:DEMOSERVER_PORT) {
     $port = $env:DEMOSERVER_PORT
 }
-
 $machine = Test-Connection $env:computername -count 1 | Select-Object Ipv4Address
 $machine = $machine.Ipv4Address
 $Command = "$env:PKG_BASE_DIR/$env:APPLICATION_NAME/netdemo-server.exe $machine $port"
