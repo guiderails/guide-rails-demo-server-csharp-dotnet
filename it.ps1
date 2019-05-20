17ae1df8e5c322dcf4e2549b03f0c6e0eb8c3cca
Adam Gensler <adam.gensler@calculi.com>
2019-05-20 11:08:38 -0700

    GX-3501 use argument

--- it.ps1
+++ it.ps1
@@ -3,8 +3,8 @@ if ($null -ne $env:DEMOSERVER.PORT) {
     $port = $env:DEMOSERVER.PORT
 }
 
-# $r = Invoke-WebRequest "http://localhost:$port/"
-$r = Invoke-WebRequest "http://localhost:8080/"
+# $r = Invoke-WebRequest -Uri "http://localhost:$port/"
+$r = Invoke-WebRequest -Uri "http://localhost:8080/"
 if (200 -ne $r.StatusCode) {
     throw "Integration test failed"
 }
