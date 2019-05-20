57f85505e0db10758663c3462a2fddb9a02ab585
Adam Gensler <adam.gensler@calculi.com>
2019-05-20 12:48:20 -0700

    GX-3501 add basic parsing

--- it.ps1
+++ it.ps1
@@ -5,7 +5,7 @@ if ($null -ne $env:DEMOSERVER.PORT) {
 
 $ipa = (Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null -and $_.NetAdapter.Status -ne "Disconnected"}).IPv4Address.IPAddress
 
-$r = Invoke-WebRequest -Uri "http://${ipa}:${port}/"
+$r = Invoke-WebRequest -Uri "http://${ipa}:${port}/" -UseBasicParsing
 if (200 -ne $r.StatusCode) {
     throw "Integration test failed"
 }
