52c30bb6a581a79fe050db67d6f7f4f39a11a8eb
Adam Gensler <adam.gensler@calculi.com>
2019-05-20 08:50:16 -0700

    GX-3501 add integration test

---
+++ netdemo-Server/Tests/ResponseGeneratorIntegration.Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Xunit;
+
+namespace Server.Integration.Tests {
+
+	public class ResponseGeneratorIntegrationTest {
+
+		readonly string url;
+
+		public ResponseGeneratorIntegrationTest() {
+			url = "http://localhost:8080/";
+		}
+
+		[Fact]
+		[Trait("Category", "Integration")]
+		void VerifyServerResponse() {
+			HttpClientHandler handler = new HttpClientHandler();
+			HttpClient httpClient = new HttpClient(handler);
+			HttpResponseMessage response;
+			response = httpClient.GetAsync(url).Result;
+			Assert.True(response.IsSuccessStatusCode);
+		}
+
+	}
+}
