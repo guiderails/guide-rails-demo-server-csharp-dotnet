2dea3871735670e8aecebb7caac914f1f613789e
Adam Gensler <adam.gensler@calculi.com>
2019-05-20 10:47:46 -0700

    GX-3501 hardcode port

--- it.ps1
+++ it.ps1
@@ -3,7 +3,8 @@ if ($null -ne $env:DEMOSERVER.PORT) {
     $port = $env:DEMOSERVER.PORT
 }
 
-$r = Invoke-WebRequest "http://localhost:$port/"
+# $r = Invoke-WebRequest "http://localhost:$port/"
+$r = Invoke-WebRequest "http://localhost:8080/"
 if (200 -ne $r.StatusCode) {
     throw "Integration test failed"
 }
