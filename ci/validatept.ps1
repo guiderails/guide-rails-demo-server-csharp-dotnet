b9bbb4b0250f8167cb74ce77f9c73428dcd6b809
Adam Gensler <adam.gensler@calculi.com>
2019-05-21 12:19:34 -0700

    GX-3509 remove slash

--- ci/validatept.ps1
+++ ci/validatept.ps1
@@ -3,7 +3,7 @@ if ($null -ne $env:PERFORMANCE_LIMIT) {
     $limit = $env:PERFORMANCE_LIMIT
 }
 
-$pt = Import-Csv -Path .\results.jtl
+$pt = Import-Csv -Path results.jtl
 $avg = ($pt | Measure-Object -Property 'elapsed' -Average).Average
 Write-Host "Average response time: $avg"
 
