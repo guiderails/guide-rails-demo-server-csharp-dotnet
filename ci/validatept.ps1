08a7feb5e70f9e8e9e43e084c73d9241c535b22a
Adam Gensler <adam.gensler@calculi.com>
2019-05-21 10:51:56 -0700

    GX-3509 validate performance test

---
+++ ci/validatept.ps1
@@ -0,0 +1,13 @@
+$limit = 10.0
+if ($null -ne $env:PERFORMANCE_LIMIT) {
+    $limit = $env:PERFORMANCE_LIMIT
+}
+
+$pt = Import-Csv -Path .\results.jtl
+$avg = ($pt | Measure-Object -Property 'elapsed' -Average).Average
+Write-Host "Average response time: $avg"
+
+if ($limit -le $avg) {
+    throw "Performance Test exceeded $limit"
+}
+Write-Host "Performance under $limit."
