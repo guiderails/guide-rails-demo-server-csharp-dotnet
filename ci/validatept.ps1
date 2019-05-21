5518918c2456ebe3a5f6e656fee171651c105536
Adam Gensler <adam.gensler@calculi.com>
2019-05-21 12:53:42 -0700

    GX-3509 address path

--- ci/validatept.ps1
+++ ci/validatept.ps1
@@ -3,7 +3,12 @@ if ($null -ne $env:PERFORMANCE_LIMIT) {
     $limit = $env:PERFORMANCE_LIMIT
 }
 
-$pt = Import-Csv -Path results.jtl
+$resultsfile = "results.jtl"
+if ($null -ne $env:TEST_LOGS_DIR) {
+    $resultsfile = "$env:TEST_LOGS_DIR/$resultsfile"
+}
+
+$pt = Import-Csv -Path $resultsfile
 $avg = ($pt | Measure-Object -Property 'elapsed' -Average).Average
 Write-Host "Average response time: $avg"
 
