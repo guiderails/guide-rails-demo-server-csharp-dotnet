4e639afe7396ab9844b828200d7ed146594482c2
Dong Hoang <dong.hoang@integnology.com>
2021-09-24 12:20:49 -0700

    Add echo command to troubleshoot failed integration test

--- ci/integration-test.ps1
+++ ci/integration-test.ps1
@@ -3,7 +3,11 @@ if ($null -ne $env:DEMOSERVER.PORT) {
     $port = $env:DEMOSERVER.PORT
 }
 
+echo $port
+
 $ipa = (Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null -and $_.NetAdapter.Status -ne "Disconnected"}).IPv4Address.IPAddress
+
+echo http://${ipa}:${port}/
 
 $r = Invoke-WebRequest -Uri "http://${ipa}:${port}/" -UseBasicParsing
 if (200 -ne $r.StatusCode) {
